26ad4521b0c6e9d3cefa7dfdeabd9df27fd9ba85
LeeCQ <lee-cq@qq.com>
2023-11-22 22:37:45 +0800

    CI update - 更新安装程序

---
+++ tests/init_alist.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")" || exit
+
+mkdir -p alist
+cd alist || exit
+
+wget https://github.com/alist-org/alist/releases/download/v3.28.0/alist-linux-amd64.tar.gz
+tar xzvf alist-linux-amd64.tar.gz
+
+./alist admin set 123456
+./alist start
