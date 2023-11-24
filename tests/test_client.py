88434e2325966db0df93434178515455ce209cbb
Lee CQ <47050568+lee-cq@users.noreply.github.com>
2023-11-24 16:22:11 +0000

    更新测试

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,8 @@
-import os
-import unittest
+import pytest
 from pathlib import Path
 
-from alist_sdk import *
+from alist_sdk import Client, Me, Item, RawItem, ListItem, Resp, Task, DirItem
 
-client = Client('http://localhost:5244', username='admin', password='123456')
 DATA_DIR = Path(__file__).parent.joinpath('alist/test_dir')
 DATA_DIR_DST = Path(__file__).parent.joinpath('alist/test_dir')
 DATA_DIR_DST.mkdir(exist_ok=True)
@@ -32,7 +30,7 @@ def create_storage_local(client_, mount_name, local_path: Path):
         "webdav_policy": "native_proxy",
         "down_proxy_url": ""
     }
-    if f'/{mount_name}' not in [i['mount_path'] for i in client.get("/api/admin/storage/list").json()['data']['content']]:
+    if f'/{mount_name}' not in [i['mount_path'] for i in client_.get("/api/admin/storage/list").json()['data']['content']]:
         assert client_.post(
             "/api/admin/storage/create",
             json=local_storage
@@ -56,111 +54,128 @@ def clear_dir(path: Path):
     path.rmdir()
 
 
-class BaseTestCase(unittest.TestCase):
+def setup_package() -> None:
+    clear_dir(DATA_DIR)
+    clear_dir(DATA_DIR_DST)
+    DATA_DIR.mkdir(exist_ok=True)
+    DATA_DIR_DST.mkdir(exist_ok=True)
+
+    _client = Client('http://localhost:5244', username='admin', password='123456')
+    create_storage_local(_client, 'local', DATA_DIR)
+    create_storage_local(_client, 'local_dst', DATA_DIR_DST)
+
+
+
+class TestSyncClient:
+
+    __client = Client('http://localhost:5244', username='admin', password='123456')
+
+    @property
+    def client(self) -> Client:
+        return self.__client
+
+
+    def setup_method(self) -> None:
+        print("--- setup function ---")
+        clear_dir(DATA_DIR)
+        clear_dir(DATA_DIR_DST)
+        DATA_DIR.mkdir(exist_ok=True)
+        DATA_DIR_DST.mkdir(exist_ok=True)
+
+    def run(self, func, *args, **kwargs):
+        return func(*args, **kwargs)
 
     def test_login(self):
         _client = Client("http://localhost:5244", verify=False)
-        self.assertTrue(_client.login(username='admin', password="123456", ), "登陆失败")
+        assert _client.login(username='admin', password="123456", ), "登陆失败"
 
     def test_me(self):
-        res = client.me()
-        self.assertEqual(res.code, 200)
-        self.assertIsInstance(res.data, Me, "数据结构错误。")
-
-
-class FSTestCase(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        create_storage_local(client, 'local', DATA_DIR)
-
-    def tearDown(self) -> None:
-        clear_dir(DATA_DIR)
-        DATA_DIR.mkdir(exist_ok=True)
+        res = self.run(self.client.me)
+        assert res.code == 200
+        assert isinstance(res.data, Me), "数据结构错误。"
+        
 
     def test_mkdir(self):
-        res = client.mkdir('/local/test_mkdir')
-        self.assertEqual(res.data, None)
-        self.assertEqual(res.code, 200)
-        self.assertTrue(DATA_DIR.joinpath('test_mkdir').exists(), "创建失败")
+        res = self.run(self.client.mkdir, '/local/test_mkdir')
+        assert res.code == 200
+        assert DATA_DIR.joinpath('test_mkdir').exists(), "创建失败"
 
     def test_rename(self):
         DATA_DIR.joinpath('test_mkdir').mkdir()
         if DATA_DIR.joinpath('test_rename').exists():
             DATA_DIR.joinpath('test_rename').rmdir()
-        res = client.rename('test_rename', '/local/test_mkdir')
-        self.assertEqual(res.data, None)
-        self.assertEqual(res.code, 200)
-        self.assertTrue(DATA_DIR.joinpath('test_rename').exists())
-        self.assertFalse(DATA_DIR.joinpath('test_mkdir').exists())
+        res =self.run(self.client.rename, 'test_rename', '/local/test_mkdir')
+        assert res.code == 200
+        assert DATA_DIR.joinpath('test_rename').exists()
+        assert not DATA_DIR.joinpath('test_mkdir').exists()
 
-    @unittest.SkipTest
+    @pytest.mark.skip
     def test_upload_file_from_data(self):
         data = b"test_data"
         alist_full_name = '/local/upload_file_form_data'
         path_file = DATA_DIR.joinpath('upload_file_form_data')
-        res = client.upload_file_form_data(data, alist_full_name, as_task=False)
-        self.assertEqual(res, None)
-        self.assertEqual(res.code, 200)
-        self.assertTrue(path_file.exists())
+        res = self.run(self.client.upload_file_form_data, data, alist_full_name, as_task=False)
+        assert res == None
+        assert res.code == 200
+        assert path_file.exists()
 
     def test_upload_put(self):
         alist_full_name = '/local/upload_file_form_data'
         alist_data_path = DATA_DIR.joinpath('upload_file_form_data')
-        res = client.upload_file_put(__file__, alist_full_name, as_task=False)
-        self.assertEqual(res.data, None)
-        self.assertEqual(res.code, 200)
-        self.assertTrue(alist_data_path)
+
+        res = self.run(self.client.upload_file_put, __file__, alist_full_name, as_task=False)
+        assert res.code == 200
+        assert alist_data_path.exists()
 
     def test_list_dir(self):
         DATA_DIR.joinpath('test_list_dir_dir').mkdir()
         DATA_DIR.joinpath('test_list_dir_file').write_text('test')
-        res = client.list_files('/local', )
-        self.assertEqual(res.code, 200)
-        self.assertIsInstance(res.data, ListItem)
-        self.assertIsInstance(res.data.content[0], Item)
+        res = self.run(self.client.list_files, '/local', )
+        assert res.code == 200
+        assert isinstance(res.data, ListItem)
+        assert isinstance(res.data.content[0], Item)
 
     def test_list_dir_null(self):
         DATA_DIR.joinpath('test_list_dir_null').mkdir()
-        res = client.list_files('/local/test_list_dir_null', )
-        self.assertEqual(res.code, 200)
-        self.assertIsInstance(res.data, ListItem)
-        self.assertEqual(res.data.total, 0)
+        res = self.run(self.client.list_files, '/local/test_list_dir_null', )
+        assert res.code == 200
+        assert isinstance(res.data, ListItem)
+        assert res.data.total == 0
 
     def test_get_item_info_dir(self):
         DATA_DIR.joinpath('test_ite_info_dir').mkdir()
-        res = client.get_item_info('/local/test_ite_info_dir')
-        self.assertEqual(res.code, 200)
-        self.assertIsInstance(res.data, RawItem)
+        res = self.run(self.client.get_item_info, '/local/test_ite_info_dir')
+        assert res.code == 200
+        assert isinstance(res.data, RawItem)
 
     def test_get_item_info_file(self):
         DATA_DIR.joinpath('test_ite_info_file.txt').write_text('abc')
-        res = client.get_item_info('/local/test_ite_info_file.txt')
-        self.assertEqual(res.code, 200)
-        self.assertIsInstance(res, Resp)
-        self.assertIsInstance(res.data, RawItem)
+        res = self.run(self.client.get_item_info, '/local/test_ite_info_file.txt')
+        assert res.code == 200
+        assert isinstance(res, Resp)
+        assert isinstance(res.data, RawItem)
 
     def test_get_dir(self):
         DATA_DIR.joinpath('test_ite_info_dir').mkdir()
 
-        res = client.get_dir('/local')
-        self.assertEqual(res.code, 200)
-        self.assertIsInstance(res.data, list)
-        self.assertIsInstance(res.data[0], DirItem)
+        res = self.run(self.client.get_dir, '/local')
+        assert res.code == 200
+        assert isinstance(res.data, list)
+        assert isinstance(res.data[0], DirItem)
 
     def test_move(self):
         DATA_DIR.joinpath('test_move_src').mkdir()
         DATA_DIR.joinpath('test_move_src/test.txt').write_text('abc')
         DATA_DIR.joinpath('test_move_dst').mkdir()
 
-        res = client.move(src_dir='/local/test_move_src',
+        res = self.run(self.client.move, src_dir='/local/test_move_src',
                           dst_dir='/local/test_move_dst',
                           files=['test.txt']
                           )
 
-        self.assertEqual(res.code, 200)
-        self.assertTrue(DATA_DIR.joinpath('test_move_dst/test.txt').exists())
-        self.assertFalse(DATA_DIR.joinpath('test_move_src/test.txt').exists())
+        assert res.code == 200
+        assert DATA_DIR.joinpath('test_move_dst/test.txt').exists()
+        assert not DATA_DIR.joinpath('test_move_src/test.txt').exists()
 
     def test_recursive_move(self):
         DATA_DIR.joinpath('test_move_src').mkdir()
@@ -169,108 +184,91 @@ class FSTestCase(unittest.TestCase):
         DATA_DIR.joinpath('test_move_src/test3.txt').write_text('abc')
         DATA_DIR.joinpath('test_move_dst').mkdir()
 
-        res = client.recursive_move(src_dir='/local/test_move_src',
+        res = self.run(self.client.recursive_move, src_dir='/local/test_move_src',
                                     dst_dir='/local/test_move_dst',
                                     )
 
-        self.assertEqual(res.code, 200)
-        self.assertTrue(DATA_DIR.joinpath('test_move_dst/test1.txt').exists())
-        self.assertFalse(DATA_DIR.joinpath('test_move_src/test1.txt').exists())
+        assert res.code == 200
+        assert DATA_DIR.joinpath('test_move_dst/test1.txt').exists()
+        assert not DATA_DIR.joinpath('test_move_src/test1.txt').exists()
 
     def test_copy(self):
         DATA_DIR.joinpath('test_move_src').mkdir()
         DATA_DIR.joinpath('test_move_src/test.txt').write_text('abc')
         DATA_DIR.joinpath('test_move_dst').mkdir()
 
-        res = client.copy(src_dir='/local/test_move_src',
+        res = self.run(self.client.copy, src_dir='/local/test_move_src',
                           dst_dir='/local/test_move_dst',
                           files=['test.txt']
                           )
 
-        self.assertEqual(res.code, 200)
-        self.assertTrue(DATA_DIR.joinpath('test_move_dst/test.txt').exists())
-        self.assertTrue(DATA_DIR.joinpath('test_move_src/test.txt').exists())
+        assert res.code == 200
+        assert DATA_DIR.joinpath('test_move_dst/test.txt').exists()
+        assert DATA_DIR.joinpath('test_move_src/test.txt').exists()
 
     def test_remove_file(self):
         DATA_DIR.joinpath('test_remove_file.txt').write_text('abc')
-        res = client.remove(path='/local',
+        res = self.run(self.client.remove, path='/local',
                             names=['test_remove_file.txt']
                             )
-        self.assertEqual(res.code, 200)
-        self.assertFalse(DATA_DIR.joinpath('test_remove_file.txt').exists())
+        assert res.code == 200
+        assert not DATA_DIR.joinpath('test_remove_file.txt').exists()
 
     def test_remove_dir_has_file(self):
         DATA_DIR.joinpath('test_dir').mkdir()
         DATA_DIR.joinpath('test_dir/test1.txt').write_text('123')
 
-        res = client.remove(path='/local',
+        res = self.run(self.client.remove, path='/local',
                             names=['test_dir']
                             )
-        self.assertEqual(res.code, 200)
-        self.assertFalse(DATA_DIR.joinpath('test_dir/test1.txt').exists())
-        self.assertFalse(DATA_DIR.joinpath('test_dir').exists())
+        assert res.code == 200
+        assert not DATA_DIR.joinpath('test_dir/test1.txt').exists()
+        assert not DATA_DIR.joinpath('test_dir').exists()
 
-    @unittest.expectedFailure
+    @pytest.mark.skip
     def test_remove_empty_directory(self):
         DATA_DIR.joinpath('test_dir').mkdir()
-        res = client.remove_empty_directory(path='/local/test_dir/')
+        res = self.run(self.client.remove_empty_directory, path='/local/test_dir/')
 
-        self.assertEqual(res.code, 200)
-        self.assertFalse(DATA_DIR.joinpath('test_dir').exists())
+        assert res.code == 200
+        assert not DATA_DIR.joinpath('test_dir').exists()
 
-    @unittest.expectedFailure
+    @pytest.mark.skip
     def test_remove_empty_directory_not(self):
         DATA_DIR.joinpath('test_dir').mkdir()
         DATA_DIR.joinpath('test_dir/test.txt').write_text('123')
 
-        res = client.remove_empty_directory(path='/local/test_dir/')
+        res = self.run(self.client.remove_empty_directory, path='/local/test_dir/')
 
-        self.assertEqual(res.code, 200)
-        self.assertFalse(DATA_DIR.joinpath('test_dir').exists())
+        assert res.code == 200
+        assert not DATA_DIR.joinpath('test_dir').exists()
 
-    @unittest.expectedFailure
+    @pytest.mark.skip
     def test_add_aria2(self):
-        res = client.add_aria2("/local", [""])
-        self.assertEqual(res.code, 200)
+        res = self.run(self.client.add_aria2, "/local", [""])
+        assert res.code == 200
 
-    @unittest.expectedFailure
+    @pytest.mark.skip
     def test_add_qbit(self):
         """"""
-        res = client.add_qbit("/local", [""])
-        self.assertEqual(res.code, 200)
+        res = self.run(self.client.add_qbit, "/local", [""])
+        assert res.code == 200
 
-
-class AdminTaskTestCase(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls) -> None:
-        create_storage_local(client, 'local', DATA_DIR)
-        create_storage_local(client, 'local_dst', DATA_DIR_DST)
-
-    def setUp(self) -> None:
-        clear_dir(DATA_DIR)
-        clear_dir(DATA_DIR_DST)
-        DATA_DIR.mkdir(exist_ok=True)
-        DATA_DIR_DST.mkdir(exist_ok=True)
 
     def test_task_done(self):
         """"""
         DATA_DIR.joinpath('test.txt').write_text('abc')
 
-        res = client.copy(src_dir='/local',
+        res = self.run(self.client.copy, src_dir='/local',
                           dst_dir='/local_dst',
                           files=['test.txt']
                           )
 
-        self.assertEqual(res.code, 200, res.message)
+        assert res.code == 200, res.message
 
-        res = client.task_done('copy')
-        self.assertEqual(res.code, 200, res.message)
-        self.assertIsInstance(res.data, list)
-        self.assertIsInstance(res.data[0], Task)
-
-
+        res = self.run(self.client.task_done, 'copy')
+        assert res.code == 200, res.message
+        assert isinstance(res.data, list)
+        assert isinstance(res.data[0], Task)
 
 
-if __name__ == '__main__':
-    unittest.main()
